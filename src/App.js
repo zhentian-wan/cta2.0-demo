761089389059419d73c0146b7e112174112af99c
zhentian-wan <answer881215@gmail.com>
2018-10-04 21:01:43 +0300

    create-react-app 2.0 svg scss css-module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo, {
+    ReactComponent as ReactLogo
+} from "./logo.svg";
+
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+            <ReactLogo className="App-logo-svg" alt="logo" />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
